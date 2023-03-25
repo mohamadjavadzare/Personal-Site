c11a474683e22da8bedce7c899ab4a97d8145c1e
Mohammad Javad Zare <mjz589.2018@gmail.com>
2023-03-26 02:45:37 +0330

    .env file created for SECRET_KEY

--- PersonalSite/settings.py
+++ PersonalSite/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c(87e(+8c&iv9rw@)*64m&gsi=6j^4&ja1kujbb5zi!xgn8$@-'
+# SECRET_KEY is in .env file
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
