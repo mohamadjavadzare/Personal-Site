582b7eda8f2d5757a3bd6f01d7f0e773fae74bb4
Mohammad Javad Zare <mjz589.2018@gmail.com>
2023-03-29 00:19:23 +0330

    Contact model - TIME_ZONE - admin.py
    changed TIME_ZONE to Asia/Tehran
    a model named Contact created
    admin.py
    made template more dynamic.

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from website.models import *
 
 # Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-'
+    list_display = ('name', 'email', 'created_date')
+    search_fields = ('name', 'email', 'message')
--- website/models.py
+++ website/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ['-created_date']
+    def __str__(self):
+        return "{} - {}".format(self.name, self.subject)
